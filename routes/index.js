c457a8df01487c659b199f2b97469dd5928c9260
guoxiu <guoxiu@globalegrow.com>
2018-07-18 09:15:51 +0800

    完成主体页面布局

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'Express' });
+  res.render('about');
+});
+
+/* GET product page. */
+router.get('/product', function(req, res, next) {
+  res.render('product');
+});
+
+/* GET news page. */
+router.get('/news', function(req, res, next) {
+  res.render('news');
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Express' });
+  res.render('contact');
 });
 
 module.exports = router;
