134c3506d15f7237b18f9224ed26f37c6ef5d665
guoxiu <guoxiu@globalegrow.com>
2018-07-23 18:09:43 +0800

    新闻数据层面

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+// 引入逻辑处理
+var newsData = require('../servers/newsData');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+    res.render('index');
 });
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('about');
+    res.render('about');
 });
 
 /* GET product page. */
 router.get('/product', function(req, res, next) {
-  res.render('product');
+    res.render('product');
 });
 
 /* GET news page. */
 router.get('/news', function(req, res, next) {
-  res.render('news');
+    res.render('news/list', newsData({
+        pageNum: req.query.page || 1,
+        size: req.query.size || 6
+    }));
+});
+
+/* GET news item page. */
+router.get('/news-item', function(req, res, next) {
+    res.render('news/item', newsData({
+        itemPage: req.query.index || 1
+    }));
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact');
+    res.render('contact');
 });
 
 module.exports = router;
